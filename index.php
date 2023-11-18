72ccc189e057e4ea305aa571391a70a775aa8a7b
keetajapan <150695247+keetajapan@users.noreply.github.com>
2023-11-19 00:02:04 +0900

    Update and rename home.php to index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php get_header();?>
+
+<body>  
+<main>
+  <section id="movecontent">
+    <div class="carousel">
+    <div><a href=""><img id="carousel1" class="carouselimg" src="<?php echo get_template_directory_uri(); ?>/image/carousel1.png" ></a></div>
+    <div><a href=""><img id="carousel2" class="carouselimg" src="<?php echo get_template_directory_uri(); ?>/image/carousel2.png" ></a></div>
+    <div><a href=""><img id="carousel3" class="carouselimg" src="<?php echo get_template_directory_uri(); ?>/image/carousel3.png" ></a></div>
+    </div>
+  </section>
+
+    <!-- main-content -->
+    <div class="main-content" id="concept">
+      <div class="left">
+        <div class="leftcontent2">
+         <h1>「ささやかな一杯で、幸せなひと時を。」</h1>
+         <br>
+         <p class="explain">「仲間と共に多くの人に幸せを届けて、仲間と共に幸せになれたらいいな」</p>
+         <p class="explain">そんな思いを詰め込んだ場所を作りました。</p>
+         <p class="explain">ささやかな一杯で幸せをお届けします。</p>
+        </div>
+        <div class="leftphoto">
+         <img  src="<?php echo get_template_directory_uri(); ?>/image/lemon.jpg" >
+        </div>
+      </div>
+      <div class="right">
+      <img  src="<?php echo get_template_directory_uri(); ?>/image/cocoa.jpg" >
+      </div>
+    </div> 
+  <br>
+  <br>
+
+    <h3 class="section-headline">各種SNSはこちら</h3>
+    <div class="sns">
+      <a href="https://www.instagram.com/"></a>
+      <a href="https://twitter.com/"></a>
+    </div>
+    <div class="snslogo">
+      <a href="https://www.instagram.com/"><img src="<?php echo get_template_directory_uri(); ?>/image/instagram.jpeg" alt="instagram"></a>
+      <a href="https://twitter.com/"><img src="<?php echo get_template_directory_uri(); ?>/image/x.jpeg" alt="x(twitter)"></a>
+    </div>
+
+    <?php get_footer();?>
+
+ 
+
+
