95bfb091e5b2e88ff769eef305f968eeaf300ed1
keetajapan <150695247+keetajapan@users.noreply.github.com>
2023-11-18 17:51:01 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,37 @@
+   <!-- footer-->
+   <footer>
+              <div class="contact">
+                <p class="title1">CONTACT</p>
+                <p>電話でのご相談は下記より</p>
+                <p>052-999-9999(10～19時定休日除)</p>
+                <p>フォームでのお問い合わせは24時間対応!</p>
+                <p><a href="index.html"><img class="contact-button" src="<?php echo get_template_directory_uri(); ?>/contact-or500.png"></a> </p>
+              </div>
+              <div class="access">
+                <p class="title2">ACCESS</p>
+                <p>〒000-000<br>  
+                 愛知県名古屋市中村区名駅南2丁目</p>
+                 <p id="info">○○公園西口正面にございます。<br>ご不明な場合はお気軽にお問合せください。</p>
+              </div>
+              <div class="map">
+                <p class="title3">MAP</p>
+                <p><iframe class="gmap" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12967.099651228138!2d139.6962182!3d35.6579169!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7abbc1a9d7d575eb!2z5qCq5byP5Lya56S-U0FNVVJBSQ!5e0!3m2!1sja!2sjp!4v1651043809252!5m2!1sja!2sjp"></iframe></p>
+              </div>
+     </footer>
+          <!-- 最後 -->
+        <div class="cr_text">
+          Copyright &copy; RCS JAPAN.
+        </div>
+     
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <!-- slickの読み込み -->
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/script111.js"></script>
+ <?php wp_footer()?>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <!-- slickの読み込み -->
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/script.js"></script>
+<?php wp_footer();?>
